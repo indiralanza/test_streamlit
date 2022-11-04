2b34dbcd9981f2db60d6d899b58298f89915d988
indiralanza <indiralanza@yahoo.es>
2022-11-04 12:21:10 +0100

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -27,8 +27,9 @@ data = load_data(10000)
 data_load_state.text("Done! (using st.cache)")
 
 
-st.subheader('Raw data')
-st.write(data)
+if st.checkbox('Show raw data'):
+    st.subheader('Raw data')
+    st.write(data)
 
 st.subheader('Number of pickups by hour')
 hist_values = np.histogram(
