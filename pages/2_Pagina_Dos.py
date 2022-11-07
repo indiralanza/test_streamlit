5930ccfdad3d04755a753c129d0ed09be3fc4167
indiralanza <indiralanza@yahoo.es>
2022-11-07 09:54:47 +0100

    Update 2_Pagina_Dos.py

--- pages/2_Pagina_Dos.py
+++ pages/2_Pagina_Dos.py
@@ -33,12 +33,7 @@ def load_data(nrows):
     return data
 
 
-# Create a text element and let the reader know the data is loading.
-data_load_state = st.text('Loading data...')
-# Load 10,000 rows of data into the dataframe.
-data = load_data(10000)
-# Notify the reader that the data was successfully loaded.
-data_load_state.text("Done!")
+
 
 
 
@@ -57,6 +52,13 @@ df = df[df["job"] == job_filter]
 
 # near real-time / live feed simulation
 for seconds in range(200):
+    
+    # Create a text element and let the reader know the data is loading.
+    data_load_state = st.text('Loading data...')
+    # Load 10,000 rows of data into the dataframe.
+    data = load_data(10000)
+    # Notify the reader that the data was successfully loaded.
+    data_load_state.text("Done!")
 
     df["age_new"] = df["age"] * np.random.choice(range(1, 5))
     df["balance_new"] = df["balance"] * np.random.choice(range(1, 5))
