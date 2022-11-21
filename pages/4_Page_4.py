38c405725f1fd20141704373f77e3327408894c0
indiralanza <indiralanza@yahoo.es>
2022-11-21 13:50:21 +0100

    Update 4_Page_4.py

--- pages/4_Page_4.py
+++ pages/4_Page_4.py
@@ -24,14 +24,9 @@ with col2:
 
 st.text(number)
 
-with st.form("my_form"):
-   st.write("Inside the form")
-   slider_val = st.slider("Form slider")
-   checkbox_val = st.checkbox("Form checkbox")
+form = st.form("my_form")
+form.slider("Inside the form")
+st.slider("Outside the form")
 
-   # Every form must have a submit button.
-   submitted = st.form_submit_button("Submit")
-   if submitted:
-       st.write("slider", slider_val, "checkbox", checkbox_val)
-
-st.write("Outside the form")
+# Now add a submit button to the form:
+form.form_submit_button("Submit")
